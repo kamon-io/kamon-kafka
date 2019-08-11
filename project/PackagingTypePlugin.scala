493f4d06d388225ca2e66015158f252ec2793c2f
Carsten Seibert <seibert@seibertec.ch>
2019-08-11 11:38:53 +0200

    Try different workaround for package.type undefined problem ...

--- project/PackagingTypePlugin.scala
+++ project/PackagingTypePlugin.scala
@@ -1,9 +1,10 @@
 import sbt._
 
 //UGLY HACK: https://github.com/sbt/sbt/issues/3618
-object PackagingTypePlugin extends AutoPlugin {
-  override val buildSettings = {
-    sys.props += "packaging.type" -> "jar"
-    Nil
-  }
-}
+//object PackagingTypePlugin extends AutoPlugin {
+//  println("====> TEST!!!!!")
+//  override val buildSettings = {
+//    sys.props += "packaging.type" -> "jar"
+//    Nil
+//  }
+//}
