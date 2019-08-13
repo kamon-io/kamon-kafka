eef7611cfab1ae53a3f8d9195ac399f4c91dfd6c
Carsten Seibert <seibert@seibertec.ch>
2019-08-13 07:57:26 +0200

    Try again another workaround ...

--- project/PackagingTypePlugin.scala
+++ project/PackagingTypePlugin.scala
@@ -1,10 +1,10 @@
 import sbt._
 
 //UGLY HACK: https://github.com/sbt/sbt/issues/3618
-//object PackagingTypePlugin extends AutoPlugin {
-//  println("====> TEST!!!!!")
-//  override val buildSettings = {
-//    sys.props += "packaging.type" -> "jar"
-//    Nil
-//  }
-//}
+object PackagingTypePlugin extends AutoPlugin {
+  println("====> PackagingTypePlugin registered")
+  override val buildSettings = {
+    sys.props += "packaging.type" -> "jar"
+    Nil
+  }
+}
