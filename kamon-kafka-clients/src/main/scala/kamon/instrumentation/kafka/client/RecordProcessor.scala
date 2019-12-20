e6d24b42dd83550f0b5a3947b2acd318e2132ea6
Mladen Subotic <mladens42@gmail.com>
2019-12-20 09:57:22 +0100

    Keep entire context in RecordProcessor

--- kamon-kafka-clients/src/main/scala/kamon/instrumentation/kafka/client/RecordProcessor.scala
+++ kamon-kafka-clients/src/main/scala/kamon/instrumentation/kafka/client/RecordProcessor.scala
@@ -4,13 +4,17 @@ import java.time.Instant
 
 import kamon.Kamon
 import kamon.context.Context
+import kamon.instrumentation.context.HasContext
 import kamon.trace.Span
 import org.apache.kafka.clients.consumer.ConsumerRecords
+
+import scala.concurrent.duration._
 
 object RecordProcessor {
 
   import scala.collection.JavaConverters._
 
+  /*TODO docs*/
   def process[V, K](startTime: Instant, clientId: String, groupId: String, records: ConsumerRecords[K, V]): ConsumerRecords[K, V] = {
 
     import Client._
@@ -32,9 +36,10 @@ object RecordProcessor {
           ContextSerializationHelper.fromByteArray(h.value())
         }.getOrElse(Context.Empty)
 
+        //TODO maybe separate metrics (counters, etc..)
         val spanBuilder = Kamon.consumerSpanBuilder("consumed-record", "kafka.consumer")
           .tagMetrics("kafka.topic", record.topic())
-          .tagMetrics("kafka.clientId", clientId)
+          .tagMetrics("kafka.clientId", clientId) //TODO not sure about these two
           .tagMetrics("kafka.groupId", groupId)
           .tag("kafka.partition", record.partition())
           .tag("kafka.offset", record.offset)
@@ -42,7 +47,7 @@ object RecordProcessor {
           .tag("kafka.timestampType", record.timestampType.name)
 
         // Key could be optional ... see tests
-        Option(record.key()).foreach(k => spanBuilder.tag("kafka.key", record.key().toString))
+        Option(record.key()).foreach(k => spanBuilder.tag("kafka.key", record.key().toString)) //TODO .toString is not wellbehaved here
 
         if (Client.followStrategy)
           spanBuilder.asChildOf(sendingContext.get(Span.Key))
@@ -52,15 +57,18 @@ object RecordProcessor {
         // Link new span also to polling span
         spanBuilder.link(pollSpan, Span.Link.Kind.FollowsFrom)
 
-        val span = if (Client.useDelayedSpans)
+        val consumedRecordSpan = if (Client.useDelayedSpans)
           // Kafka's timestamp is expressed in millis, convert to nanos => this might spoil precision here ...
-          spanBuilder.delay(Kamon.clock().toInstant(record.timestamp() * 1000 * 1000)).start(startTime)
+          spanBuilder.delay(Kamon.clock().toInstant(record.timestamp().millis.toNanos)).start(startTime)
         else
           spanBuilder.start(startTime)
 
         // Complete the span and store it on the record (using the HasSpan mixin)
-        span.finish
-        record.setContext(Context.of(Span.Key, span))
+        consumedRecordSpan.finish
+
+        record
+          .asInstanceOf[HasContext]
+          .setContext(sendingContext.withEntry(Span.Key, consumedRecordSpan))
       }
     }
     records
